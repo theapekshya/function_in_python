0deaaefc8ab99fe48c27869289e70df522153774
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-14 19:56:13 +0545

    function_in_python

---
+++ function1.py
@@ -0,0 +1,12 @@
+def display():
+  print('python function')
+display()  #function call
+
+
+#def msg():
+ #   a=input('enter name')
+  #  print('dear ',a,'welcome to python programming')
+#msg()
+
+
+
